d2cc460dc5ff3e97effabca5e106be796cf452d8
Jetmate <antonoutkine@gmail.com>
2017-09-17 15:37:50 -0500

    Standardize style, add more content

--- src/pages/main/component/Main.jsx
+++ src/pages/main/component/Main.jsx
@@ -5,36 +5,52 @@ class Component extends React.Component {
   render () {
     return (
       <div className={style.main}>
+        <img src="media/ncplogo.svg" style={{width:'500px', margin: '50px auto'}} className="horizontal-center" />
+        <div className="row">
+          <a href="https://github.com/NCPCode"><img src="media/github.svg" style={{height:'50px'}} /></a>
+          <a href="https://www.facebook.com/groups/1586369904743034/"><img src="media/facebook.svg" style={{height:'50px'}} /></a>
+        </div>
         <h1 id="summary">summary</h1>
-        <div className="row captions">
+        <div className={"row centered"}>
           <div className="el-3">
-            <img src="media/question.svg" style={{width:'40%'}} />
+            <img src="media/question1.svg" style={{maxWidth:'300px'}} />
             <p>you know nothing about computers</p>
           </div>
-          <img src="media/arrow.svg" style={{width:'20%'}} />
+          <img src="media/arrow.svg" style={{maxWidth:'250px'}} />
           <div className="el-3">
-            <img src="media/programmer.svg" style={{width:'40%'}} />
+            <img src="media/programmer.svg" style={{maxWidth:'300px'}} />
             <p>you can make websites and cool games</p>
           </div>
         </div>
-        <h1 id="philosophies">our core philosophies</h1>
-        <div className="row captions">
-          <div className="el-3">
-            <img src="media/snakes.svg" style={{width:'40%'}} />
-            <p><b>simple</b></p>
-            <p>we use the simplest tools to insure that you understand the topic</p>
+
+        <h1>coding can be fun!</h1>
+        <p>Coding is notorious for being the favorite pastime of 30-year-olds who live in their parents' basements, but that couldn't be farther from the truth. We will prove that coding is actually...</p>
+        <div className="row centered">
+          <div>
+            <h3>simple</h3>
+            <p>Not all coding is like Java - languages like Python are very readable and easy to pick up</p>
           </div>
-          <div className="el-3">
-            <img src="media/clock.svg" style={{width:'40%'}} />
-            <p><b>quick</b></p>
-            <p>we focus on efficiency, making learning new concepts a breeze</p>
-          </div>
-          <div className="el-3">
-            <img src="media/fist.svg" style={{width:'40%'}} />
-            <p><b>fun</b></p>
-            <p>we teach languages that you actually have fun with!</p>
+          <div>
+            <h3>fun</h3>
+            <p>We will teach tools that enable you to actually make fun things with coding, like websites and video games</p>
           </div>
         </div>
+
+        <h1 id="schedule">we meet Fridays!</h1>
+        <img src="media/time.svg" style={{width: '100%'}} />
+        <h1>schedule for first 5 days</h1>
+        <img src="media/schedule.svg" style={{width: '100%'}} />
+
+        <h1 id="topics">topics</h1>
+        <h2>Python</h2>
+        <p>we'll kick off our club with an exploration of Python and its potential in <b>video-game making</b></p>
+        <h2>Web development</h2>
+        <p>in January we'll start exploring the vast amount of tools availabe to web designers, beyond just plain HTML/CSS/JS</p>
+        <p>we will also focus on teaching good <b>design principles</b></p>
+        <h2>after that, who knows? more fun coding :)</h2>
+
+        <h1 id="team">team</h1>
+        <h2>Anton Outkine</h2>
       </div>
     )
   }
--- src/pages/main/component/Sidebar.jsx
+++ src/pages/main/component/Sidebar.jsx
@@ -6,9 +6,11 @@ class Component extends React.Component {
     return (
       <div>
         <div className={style.sideBar}>
-          <div className={style.title}>NCP Code</div>
-          <div className={style.links}>
-            <a href="#philosophies">focus</a>
+          <a href="#" className={style.title}>NCP Code</a>
+          <div className={style.links + ' row'}>
+            <a href="#team">team</a>
+            <a href="#topics">topics</a>
+            <a href="#schedule">schedule</a>
             <a href="#summary">summary</a>
           </div>
         </div>
